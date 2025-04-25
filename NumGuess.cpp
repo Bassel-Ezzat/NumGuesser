8857c3919c61cdf382499cf56bc15ca7bfba3a8c
Bassel Ezzat <Bassel.a.ezzat@gmail.com>
2025-04-25 19:15:12 +0300

    Second commit.
    Fixed some issues and did some modifications and replaced repeated code with a made function

--- NumGuess.cpp
+++ NumGuess.cpp
@@ -2,140 +2,121 @@
 #include <cstdlib>
 #include <ctime>
 using namespace std;
+bool Pagain();
 int main() {
     bool playagain = true;
     while (playagain) {
-        cout << "Choose difficulty :\n" << "1:Easy (From 1 to 10 in 3 guesses maximum)\n" << "2:Medium (From 1 to 100 in 7 guesses maximum)\n"
+        cout << "Choose difficulty :\n" 
+        << "1:Easy (From 1 to 10 in 3 guesses maximum)\n" 
+        << "2:Medium (From 1 to 100 in 7 guesses maximum)\n"
         << "3:Hard (From 1 to 1000 in 15 guesses maximum)\n";
         srand(time(NULL));
-        int r1 = rand() % 11;
-        int r2 = rand() % 101;
-        int r3 = rand() % 1001;
-        int gcount = 0;
-        int x;
-        int diff;
+        int r1 = rand() % 10 + 1;
+        int r2 = rand() % 100 + 1;
+        int r3 = rand() % 1000 + 1;
+        int gcount = 0, x, diff;
+        bool z = true;
         char YorN;
         cin >> diff;
         while(true) {
-            if (diff==1) {
-                cout << "Enter a number Between 0 and 10!" << endl;
+            if (diff == 1) {
+                if (z){
+                cout << "Enter a number Between 1 and 10!" << endl;
+                z = false;
+                }
                 cin >> x;
                 gcount++;
-                if (x==r1) {
+                if (x == r1) {
                     cout << "You win!" << endl;
                     cout << "You guessed the number in " << gcount << " attempts!" << endl;
-                    cout << "Want to play again? Y=yes N=no\n";
-                    cin >> YorN;
-                    while (YorN!='N' && YorN!='Y') {
-                        cout << "Enter Y or N to proceed" << endl;
-                        cin >> YorN;
-                    }
-                    if (YorN=='N') {
-                        playagain = false;
-                    }
+                    playagain = Pagain();
                     break;
                 }
-                else if (x>r1) {
-                    cout << "Lower" << endl;
+                if (gcount >= 3) {
+                    cout << "You lose!" << endl << "The correct number was : " << r1 << " ." << endl;
+                    playagain = Pagain();
+                    break;
                 }
-                else if (x<r1) {
-                    cout << "Higher" << endl;
+                else if (x > r1) {
+                    cout << "Lower!" << endl;
                 }
-                if (gcount>=3) {
-                    cout << "You lose! The correct number was : " << r1 << endl;
-                    cout << "Want to play again? Y=yes N=no\n";
-                    cin >> YorN;
-                    while (YorN!='N' && YorN!='Y') {
-                        cout << "Enter Y or N to proceed" << endl;
-                        cin >> YorN;
-                    }
-                    if (YorN=='N') {
-                    playagain = false;
-                    }
+                else if (x < r1) {
+                    cout << "Higher!" << endl;
+                }
+            }
+            else if (diff == 2) {
+                if (z){
+                cout << "Enter a number Between 1 and 100!" << endl;
+                z = false;
+                }
+                cin >> x;
+                gcount++;
+                if (x == r2) {
+                    cout << "You win!" << endl;
+                    cout << "You guessed the number in " << gcount << " attempts!" << endl;
+                    playagain = Pagain();
+                    break;
+                }
+                else if (x > r2) {
+                    cout << "Lower!" << endl;
+                }
+                else if (x < r2) {
+                    cout << "Higher!" << endl;
+                }
+                if (gcount >= 7) {
+                    cout << "You lose!" << endl << "The correct number was : " << r2 << " ."  << endl;
+                    playagain = Pagain();
                     break;
                 }
             }
-            else if (diff==2) {
-                cout << "Enter a number Between 0 and 100!" << endl;
+            else if (diff == 3) {
+                if (z){
+                cout << "Enter a number Between 1 and 1000!" << endl;
+                z = false;
+                }
                 cin >> x;
                 gcount++;
-                if (x==r2) {
+                if (x == r3) {
                     cout << "You win!" << endl;
                     cout << "You guessed the number in " << gcount << " attempts!" << endl;
-                    cout << "Want to play again? Y=yes N=no\n";
-                    cin >> YorN;
-                    while (YorN!='N' && YorN!='Y') {
-                        cout << "Enter Y or N to proceed" << endl;
-                        cin >> YorN;
-                    }
-                    if (YorN=='N') {
-                        playagain = false;
-                    }
+                    playagain = Pagain();
+                }
+                else if (x > r3) {
+                    cout << "Lower!" << endl;
+                }
+                else if (x < r3) {
+                    cout << "Higher!" << endl;
+                }
+                if (gcount >= 15) {
+                    cout << "You lose!" << endl <<"The correct number was : " << r3 << " ."  << endl;
+                    playagain = Pagain();
                     break;
-                }
-                else if (x>r2) {
-                    cout << "Lower" << endl;
-                }
-                else if (x<r2) {
-                    cout << "Higher" << endl;
-                }
-                if (gcount>=7) {
-                    cout << "You lose! The correct number was : " << r2 << endl;
-                    cout << "Want to play again? Y=yes N=no\n";
-                    cin >> YorN;
-                    while (YorN!='N' && YorN!='Y') {
-                        cout << "Enter Y or N to proceed" << endl;
-                        cin >> YorN;
-                    }
-                    if (YorN=='N') {
-                        playagain = false;
-                    }
-                    break;
-                }
-            }
-            else if (diff==3) {
-                cout << "Enter a number Between 0 and 1000!" << endl;
-                cin >> x;
-                gcount++;
-                if (x==r3) {
-                    cout << "You win!" << endl;
-                    cout << "You guessed the number in " << gcount << " attempts!" << endl;
-                    cout << "Want to play again? Y=yes N=no\n";
-                    cin >> YorN;
-                    while (YorN!='N' && YorN!='Y') {
-                        cout << "Enter Y or N to proceed" << endl;
-                        cin >> YorN;
-                    }
-                    if (YorN=='N') {
-                        playagain = false;
-                    }
-                    break;
-                }
-                else if (x>r3) {
-                    cout << "Lower" << endl;
-                }
-                else if (x<r3) {
-                    cout << "Higher" << endl;
                 }
             }
             else {
                 cout << "Enter a number Between 1 and 3 to play!" << endl;
                 cin >> diff;
             }
-            if (gcount>=15) {
-                cout << "You lose! The correct number was : " << r3 << endl;
-                cout << "Want to play again? Y=yes N=no\n";
-                cin >> YorN;
-                while (YorN!='N' && YorN!='Y') {
-                    cout << "Enter Y or N to proceed" << endl;
-                    cin >> YorN;
-                }
-                if (YorN=='N') {
-                    playagain = false;
-                }
-                break;
-            }
+            
         }
     }
     return 0;
+}
+
+
+
+bool Pagain() {
+    char YorN;
+    cout << "Want to play again? Y/N\n";
+    cin >> YorN;
+
+    while (YorN != 'N' && YorN != 'n' && YorN != 'Y' && YorN != 'y') {
+        cout << "Enter Y or N to proceed" << endl;
+        cin >> YorN;
+    }
+
+    if (YorN == 'N' || YorN == 'n') {
+        return false; 
+    }
+    return true; 
 }
